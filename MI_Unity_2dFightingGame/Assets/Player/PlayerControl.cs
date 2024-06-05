2123c1fa8273fb53358e340c6ac47cdd45610939
Akilis18 <130033045+Akilis18@users.noreply.github.com>
2024-06-05 21:02:50 +0800

    Update
    1. Update key to interrupt game
    2. Rewrite player movement: switch to GetKeyDown function
    3. Remove bool input method verifying

--- MI_Unity_2dFightingGame/Assets/Player/PlayerControl.cs
+++ MI_Unity_2dFightingGame/Assets/Player/PlayerControl.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -21,6 +22,9 @@ public class PlayerControl : MonoBehaviour
     private float hitTm;
 
     public GDTFadeEffect GDT;
+
+    Dictionary<string, bool> HoldingKey = new Dictionary<string, bool>();
+
     #endregion
  
     void Start()
@@ -34,11 +38,20 @@ public class PlayerControl : MonoBehaviour
 
         timer = 0;
         hitTm = 0;
+
+        HoldingKey.Add("RightArr", false);
+        HoldingKey.Add("LeftArr", false);
     }
  
 
     void Update()
     {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Debug.Log("Game Interrupted");
+            SceneManager.LoadScene("StartScene");
+        }
+
         if (HP <= 0)
         {
             Debug.Log("Game over");
@@ -63,139 +76,143 @@ public class PlayerControl : MonoBehaviour
         //Always get the state of the animator
         playerSInfo = playerANI.GetCurrentAnimatorStateInfo(0);
 
-        if (!boolInputMethod)
+        #region [Input: Keyboard]
+        //Change animator state by x velocity
+        if (hitTm == 0f)
         {
-            #region [Input: Keyboard]
-            //Change animator state by x velocity
-            if (hitTm == 0f)
+            if (playerSInfo.IsName("PlayerStand") || playerSInfo.IsName("PlayerWalk") || playerSInfo.IsName("PlayerRun"))
             {
-                if (playerSInfo.IsName("PlayerStand") || playerSInfo.IsName("PlayerWalk") || playerSInfo.IsName("PlayerRun"))
+                if (playerRB.velocity.x <= -3f)
                 {
-                    if (playerRB.velocity.x <= -3f)
-                    {
-                        playerANI.Play("PlayerRun");
-                        playerSR.flipX = true;
-                    }
-
-                    if (playerRB.velocity.x <= -0.1f && playerRB.velocity.x > -3f)
-                    {
-                        playerANI.Play("PlayerWalk");
-                        playerSR.flipX = true;
-                    }
-
-                    if (playerRB.velocity.x <= 0.1f && playerRB.velocity.x > -0.1f)
-                    {
-                        playerANI.Play("PlayerStand");
-                    }
-
-                    if (playerRB.velocity.x <= 3f && playerRB.velocity.x > 0.1f)
-                    {
-                        playerANI.Play("PlayerWalk");
-                        playerSR.flipX = false;
-                    }
-
-                    if (playerRB.velocity.x > 3f)
-                    {
-                        playerANI.Play("PlayerRun");
-                        playerSR.flipX = false;
-                    }
+                    playerANI.Play("PlayerRun");
+                    playerSR.flipX = true;
                 }
-            }
 
+                if (playerRB.velocity.x <= -0.1f && playerRB.velocity.x > -3f)
+                {
+                    playerANI.Play("PlayerWalk");
+                    playerSR.flipX = true;
+                }
 
-            //Change animator state by y velocity
-            if (playerSInfo.IsName("PlayerJump"))
-            {
-                if (Mathf.Abs(playerRB.velocity.y) <= 0.00001f)
+                if (playerRB.velocity.x <= 0.1f && playerRB.velocity.x > -0.1f)
                 {
                     playerANI.Play("PlayerStand");
                 }
-            }
 
-            //increase velocity when press L/R key down
-            if (hitTm == 0f)
-            {
-                if (!playerSInfo.IsName("PlayerJump") && !playerSInfo.IsName("PlayerDuck"))
+                if (playerRB.velocity.x <= 3f && playerRB.velocity.x > 0.1f)
                 {
-                    if (Input.GetKey(KeyCode.RightArrow) && playerRB.velocity.x <= 7f)
-                    {
-                        playerRB.AddForce(new Vector2(1000f * Time.deltaTime, 0), ForceMode2D.Force);
-                    }
-                    if (Input.GetKey(KeyCode.LeftArrow) && playerRB.velocity.x >= -7f)
-                    {
-                        playerRB.AddForce(new Vector2(-1000f * Time.deltaTime, 0), ForceMode2D.Force);
-                    }
+                    playerANI.Play("PlayerWalk");
+                    playerSR.flipX = false;
                 }
-                else
+
+                if (playerRB.velocity.x > 3f)
                 {
-                    //Change the direction of player
-                    if (playerRB.velocity.x > 0.1f)
-                        playerSR.flipX = false;
-
-                    if (playerRB.velocity.x < -0.1f)
-                        playerSR.flipX = true;
-
-                    //Change velocity when get key
-                    if (Input.GetKey(KeyCode.RightArrow))
-                    {
-                        playerRB.velocity = new Vector2(5f, playerRB.velocity.y);
-                    }
-                    if (Input.GetKey(KeyCode.LeftArrow))
-                    {
-                        playerRB.velocity = new Vector2(-5f, playerRB.velocity.y);
-                    }
+                    playerANI.Play("PlayerRun");
+                    playerSR.flipX = false;
                 }
             }
+        }
 
-            //Jump
-            if (hitTm == 0f && !playerSInfo.IsName("PlayerJump") && Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                playerANI.Play("PlayerJump");
-                playerRB.AddForce(new Vector2(0f, 7f), ForceMode2D.Impulse);
-            }
 
-            //Duck
-            if (hitTm == 0f && !playerSInfo.IsName("PlayerDuck") && Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                playerANI.Play("PlayerDuck");
-                playerRB.AddForce(new Vector2(0f, -7f), ForceMode2D.Impulse);
-            }
-            if (playerSInfo.IsName("PlayerDuck") && Input.GetKeyUp(KeyCode.DownArrow))
+        //Change animator state by y velocity
+        if (playerSInfo.IsName("PlayerJump"))
+        {
+            if (Mathf.Abs(playerRB.velocity.y) <= 0.00001f)
             {
                 playerANI.Play("PlayerStand");
             }
+        }
 
-            //Attack
-            if ((hitTm == 0f && !playerSInfo.IsName("PlayerChop") && !playerSInfo.IsName("PlayerCutU")) && Input.GetKeyDown(KeyCode.Space))
+        //increase velocity when press L/R key down
+        if (hitTm == 0f)
+        {
+            if (!playerSInfo.IsName("PlayerJump") && !playerSInfo.IsName("PlayerDuck"))
             {
-                if (playerSR.flipX)
-                {
-                    //flip sword
-                    gameObject.transform.GetChild(0).gameObject.transform.localScale = new Vector2(-1, 1);
-                }
-                else
-                {
-                    gameObject.transform.GetChild(0).gameObject.transform.localScale = new Vector2(1, 1);
-                }
+                if (Input.GetKeyDown(KeyCode.RightArrow)) HoldingKey["RightArr"] = true;
+                if (Input.GetKeyDown(KeyCode.LeftArrow)) HoldingKey["LeftArr"] = true;
+                if (Input.GetKeyUp(KeyCode.RightArrow)) HoldingKey["RightArr"] = false;
+                if (Input.GetKeyUp(KeyCode.LeftArrow)) HoldingKey["LeftArr"] = false;
 
-                int moveChoice = Random.Range(1, 3);
-                switch (moveChoice)
+
+                if (HoldingKey["RightArr"] && playerRB.velocity.x <= 7f)
                 {
-                    case 1:
-                        playerANI.Play("PlayerChop");
-                        break;
-                    case 2:
-                        playerANI.Play("PlayerCutU");
-                        break;
+                    playerRB.AddForce(new Vector2(1000f * Time.deltaTime, 0), ForceMode2D.Force);
+                }
+                if (HoldingKey["LeftArr"] && playerRB.velocity.x >= -7f)
+                {
+                    playerRB.AddForce(new Vector2(-1000f * Time.deltaTime, 0), ForceMode2D.Force);
                 }
             }
+            else
+            {
+                //Change the direction of player
+                if (playerRB.velocity.x > 0.1f)
+                    playerSR.flipX = false;
 
-            #endregion
+                if (playerRB.velocity.x < -0.1f)
+                    playerSR.flipX = true;
+
+                if (Input.GetKeyDown(KeyCode.RightArrow)) HoldingKey["RightArr"] = true;
+                if (Input.GetKeyDown(KeyCode.LeftArrow)) HoldingKey["LeftArr"] = true;
+                if (Input.GetKeyUp(KeyCode.RightArrow)) HoldingKey["RightArr"] = false;
+                if (Input.GetKeyUp(KeyCode.LeftArrow)) HoldingKey["LeftArr"] = false;
+
+                //Change velocity when get key
+                if (HoldingKey["RightArr"])
+                {
+                    playerRB.velocity = new Vector2(5f, playerRB.velocity.y);
+                }
+                if (HoldingKey["LeftArr"])
+                {
+                    playerRB.velocity = new Vector2(-5f, playerRB.velocity.y);
+                }
+            }
         }
-        else
+
+        //Jump
+        if (hitTm == 0f && !playerSInfo.IsName("PlayerJump") && Input.GetKeyDown(KeyCode.UpArrow))
         {
-            //TODO: input by kinect
+            playerANI.Play("PlayerJump");
+            playerRB.AddForce(new Vector2(0f, 7f), ForceMode2D.Impulse);
         }
+
+        //Duck
+        if (hitTm == 0f && !playerSInfo.IsName("PlayerDuck") && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            playerANI.Play("PlayerDuck");
+            playerRB.AddForce(new Vector2(0f, -7f), ForceMode2D.Impulse);
+        }
+        if (playerSInfo.IsName("PlayerDuck") && Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            playerANI.Play("PlayerStand");
+        }
+
+        //Attack
+        if ((hitTm == 0f && !playerSInfo.IsName("PlayerChop") && !playerSInfo.IsName("PlayerCutU")) && Input.GetKeyDown(KeyCode.Space))
+        {
+            if (playerSR.flipX)
+            {
+                //flip sword
+                gameObject.transform.GetChild(0).gameObject.transform.localScale = new Vector2(-1, 1);
+            }
+            else
+            {
+                gameObject.transform.GetChild(0).gameObject.transform.localScale = new Vector2(1, 1);
+            }
+
+            int moveChoice = Random.Range(1, 3);
+            switch (moveChoice)
+            {
+                case 1:
+                    playerANI.Play("PlayerChop");
+                    break;
+                case 2:
+                    playerANI.Play("PlayerCutU");
+                    break;
+            }
+        }
+
+        #endregion
     }
 
     //Fall out of the game field
